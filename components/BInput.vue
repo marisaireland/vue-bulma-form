3fdd087a98d45ae1857f160ad03f6690bb48df13
Marisa Ireland <mbireland18@gmail.com>
2021-05-11 18:19:48 -0600

    create input component

---
+++ components/BInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <label
+      :for="_uid"
+      class="label"
+    >
+      <slot name="label" />
+    </label>
+    <div class="control">
+      <input
+        :id="_uid"
+        class="input"
+        type="text"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BInput',
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
---
+++ index.js
@@ -0,0 +1,9 @@
+import BInput from './components/BInput.vue';
+
+const VueBulmaForm = {
+  install(Vue) {
+    Vue.component(BInput.name, BInput);
+  }
+};
+
+export default VueBulmaForm;
